ef9791b63d9b17f1f4cd784125c5006be248b459
s0miiiik <145026691+s0miiiik@users.noreply.github.com>
2023-09-26 09:52:55 +0300

    Add files via upload

---
+++ lab1.sql
@@ -0,0 +1,9 @@
+1)SELECT population  FROM cities  LIMIT 10  ;  
+
+2)SELECT id,name,population,region  FROM cities  ORDER BY name DESC LIMIT 30;
+
+3)SELECT *   FROM cities  ORDER BY region, population DESC;
+
+4)SELECT DISTINCT region  FROM cities;
+
+5)SELECT *  FROM cities  ORDER BY region DESC,name DESC;
